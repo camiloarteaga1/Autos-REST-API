ce0fea55bb5714e5a468e0f0f30b437884286c37
camiloarteaga1 <camilo.arteaga1@udea.edu.co>
2024-06-08 03:29:06 +0200

    Server and docker file started

--- backend/main.go
+++ backend/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	//"encoding/json"
+
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
 
@@ -13,18 +15,18 @@ func main() {
 	//db.DB_Conection()
 
 	// Router setup
-	router := NewRouter()
+	router := mux.NewRouter()
 
 	// CORS setup
 	c := cors.New(cors.Options{
-		AllowedOrigins:     []string{"http://localhost:5432"},
-		AllowedCredentials: true,
-		AllowedMethods:     []string{"GET", "POST", "DELETE", "PUT"},
+		AllowedOrigins:   []string{"http://localhost:8080"},
+		AllowCredentials: true,
+		AllowedMethods:   []string{"GET", "POST", "DELETE", "PUT"},
 	})
 
 	handler := c.Handler(router)
 
 	// Start server
-	log.Fatal(http.ListenAndServe(":5432", handler))
-	log.Println("Server running on port 5432")
+	log.Fatal(http.ListenAndServe(":8080", handler))
+	log.Println("Server running on port 8080")
 }
